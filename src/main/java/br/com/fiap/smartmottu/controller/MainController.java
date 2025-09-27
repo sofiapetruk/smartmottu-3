8857fa5a2b9f17ad3daae8806b6bcce397b46608
sofia.petruk <sofia.petruk@castecnologia.com.br>
2025-09-26 23:30:46 -0300

    Adicionando a página inicial

---
+++ src/main/java/br/com/fiap/smartmottu/controller/MainController.java
@@ -0,0 +1,16 @@
+package br.com.fiap.smartmottu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class MainController {
+
+    @GetMapping()
+    public String index() {
+        return "home";
+    }
+
+}
